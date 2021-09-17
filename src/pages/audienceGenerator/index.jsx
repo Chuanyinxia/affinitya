5f5ae27cefa318fafc19edb3091f298f2994cc55
Dongziwei <ziwei.dong@hcytech.com.cn>
2021-09-17 11:50:45 +0800

    change ui input

--- src/pages/audienceGenerator/index.jsx
+++ src/pages/audienceGenerator/index.jsx
@@ -2,34 +2,101 @@ import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {httpLoading} from '@/store/actions';
-import {Button, Card, Col, Divider, Form, Input, InputNumber, Radio, Row, Select, Space, Spin} from 'antd';
+import {Button, Card, Col, Divider, Form, Input, InputNumber, message, Radio, Row, Select, Space, Spin} from 'antd';
 import './style.css';
 
-import {InfoCircleOutlined} from '@ant-design/icons';
+import {InfoCircleOutlined, PlusOutlined} from '@ant-design/icons';
+import {Countrys} from '@/components/plugin/Country';
+import {get, post} from '@/utils/request';
+import {GETAUDIENCEID, ISPAID} from '@/api';
 
 
 const AudienceGenerator = ({userInfo, httpLoading, setHttpLoading}) => {
   const [loading, setLoading] = useState(false);
-
-
+  const [audienceID, setAudienceID] = useState('');
+  const [audienceIdItem, setAudienceIdItem] = useState([]);
+  const [isPayUser, setIsPayUser] = useState(false);
+  const [searchType, setSearchType] = useState(1);
   // new
   const [startForm] = Form.useForm();
   const [baseForm] = Form.useForm();
+  const [baseFormRadio] = Form.useForm();
+  const [keyWordForm] = Form.useForm();
+  const [lookalikeForm] = Form.useForm();
 
-  // const isPay=()=>{
-  //   get(ISPAID, userInfo.token).then((res)=>{
-  //     setIsPayUser(res.data===2);
-  //   }).catch((error)=>{
-  //     message.error({
-  //       content: error.toString(), key: 'netError', duration: 2,
-  //     });
-  //   });
-  // };
+
+  const addItem = () => {
+    if (audienceID) {
+      audienceIdItem.push({audienceId: audienceID});
+      setAudienceIdItem(audienceIdItem);
+      setAudienceID(null);
+    }
+  };
+  const onLKSearchChange = (changedValues, allValues) => {
+    const adAccountId = allValues.adAccountId;
+    const accessToken = allValues.accessToken;
+    const myAppId = allValues.myAppId;
+    const myAppSecret = allValues.myAppSecret;
+    if (Object.keys(changedValues) !== 'audienceId' &&
+      adAccountId &&
+      myAppSecret &&
+      myAppId &&
+      myAppSecret
+    ) {
+      const data = {
+        adAccountId: adAccountId,
+        accessToken: accessToken,
+        myAppId: myAppId,
+        myAppSecret: myAppSecret,
+      };
+      post(GETAUDIENCEID, data, {
+        'token': userInfo.token,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }).then((res) => {
+        setAudienceIdItem(res.data);
+      }).catch((error) => {
+        message.error({
+          content: error.toString(), key: 'netError', duration: 2,
+        });
+      });
+    }
+  };
+
+  const isPay = () => {
+    get(ISPAID, userInfo.token).then((res) => {
+      setIsPayUser(res.data === 2);
+    }).catch((error) => {
+      message.error({
+        content: error.toString(), key: 'netError', duration: 2,
+      });
+    });
+  };
+  const onSearchTypeChange = (e) => {
+    setSearchType(e.target.value);
+  };
 
 
   useEffect(() => {
     setLoading(false);
-    // isPay()
+    isPay();
+    if (userInfo.adAccountId && userInfo.accessToken && userInfo.myAppId && userInfo.myAppSecret) {
+      const sdata = {
+        adAccountId: userInfo.adAccountId,
+        accessToken: userInfo.accessToken,
+        myAppId: userInfo.myAppId,
+        myAppSecret: userInfo.myAppSecret,
+      };
+      post(GETAUDIENCEID, sdata, {
+        'token': userInfo.token,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }).then((res) => {
+        setAudienceIdItem(res.data);
+      }).catch((error) => {
+        message.error({
+          content: error.toString(), key: 'netError', duration: 2,
+        });
+      });
+    }
   },
   []);
   const keywords = ['Basketball', 'Tennis', 'Dancing', 'Assemblage', 'Real Estate', 'Urna', 'Bibendum', 'Pellentesque'];
@@ -74,49 +141,85 @@ const AudienceGenerator = ({userInfo, httpLoading, setHttpLoading}) => {
                 <Form.Item
                   name="country"
                   label="Country"
+                  initialValue={['US']}
                 >
-                  <Select mode="tags"/>
-                </Form.Item>
-                <Form.Item
-                  name="accessToken"
-                  label="Access Token"
-                  tooltip={{
-                    title: 'Tooltip with customize icon',
-                    icon: <InfoCircleOutlined/>,
-                  }}>
-                  <Input/>
+                  <Select
+                    mode="multiple"
+                    showSearch
+                    maxTagCount={6}
+                    maxTagTextLength={30}
+                    optionFilterProp="children"
+                    filterOption={(input, option) =>
+                      option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }
+                    filterSort={(optionA, optionB) =>
+                      optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+                    }
+                  >
+                    {Countrys.map((item) => (
+                      <Select.Option key={item.country_code} value={item.country_code}>{item.name}</Select.Option>
+                    ))}
+                  </Select>
                 </Form.Item>
                 <Form.Item label="Age">
-                  <Input.Group compact>
-                    <Form.Item name={['age', 'min']}>
-                      <InputNumber/>
-                    </Form.Item>
-                    <Form.Item name={['age', 'max']}>
-                      <InputNumber/>
-                    </Form.Item>
-                  </Input.Group>
+                  <Row>
+                    <Col span={10}>
+                      <Form.Item name={['age', 'min']} initialValue={13}>
+                        <InputNumber className="width40"
+                          min={13}
+                          max={65}
+                          decimalSeparator={0}/>
+                      </Form.Item>
+                    </Col>
+                    <Col span={4} className="ages text-center">-</Col>
+                    <Col span={10}>
+                      <Form.Item name={['age', 'max']} initialValue={65}>
+                        <InputNumber className="width40" min={13}
+                          max={65}
+                          decimalSeparator={0}/>
+                      </Form.Item>
+                    </Col>
+                  </Row>
                 </Form.Item>
-                <Form.Item label="Gender" name="gender">
-                  <Radio.Group>
-                    <Radio value="1, 2">All</Radio>
+              </Form>
+              <Form
+                form={baseFormRadio}
+                name="segmentsRadioInfo"
+                labelAlign="left"
+                labelCol={{
+                  span: 6,
+                }}
+                wrapperCol={{
+                  span: 16,
+                }}>
+                <Form.Item label="Gender" name="gender" initialValue={'1,2'}>
+                  <Radio.Group size="large">
+                    <Radio value="1,2">All</Radio>
                     <Radio value="1">Male</Radio>
                     <Radio value="2">Female</Radio>
                   </Radio.Group>
                 </Form.Item>
-                <Form.Item label="Language" name="language">
-                  <Radio.Group>
+                <Form.Item
+                  label="Language"
+                  name="language"
+                  initialValue={'en_US'}>
+                  <Radio.Group size="large">
                     <Radio value="en_US">en_US</Radio>
                     <Radio value="zh_CN">zh_CN</Radio>
                   </Radio.Group>
                 </Form.Item>
-                <Form.Item label="User OS" name="os">
-                  <Radio.Group>
+                <Form.Item
+                  label="User OS"
+                  name="os"
+                  initialValue={'na'}>
+                  <Radio.Group size="large">
                     <Radio value="na">All</Radio>
                     <Radio value="iOS">IOS</Radio>
                     <Radio value="Android">Android</Radio>
                   </Radio.Group>
                 </Form.Item>
-                <Form.Item label="Device Platform" name="platform">
+                <Form.Item label="Device Platform" name="platform"
+                  initialValue={'all'}>
                   <Radio.Group>
                     <Radio value="all">All</Radio>
                     <Radio value="mobile">Mobile</Radio>
@@ -127,7 +230,87 @@ const AudienceGenerator = ({userInfo, httpLoading, setHttpLoading}) => {
             </Card>
             <Card className="marginB32 padding64" hoverable>
               <h2>3. Last small step to get your generated keywords</h2>
-              <h4>Input one or more keywords, it is suggested to try different combinations.</h4>
+              <h4 className="marginB64">Input one or more keywords, it is suggested to try different combinations.</h4>
+              <Radio.Group onChange={onSearchTypeChange} value={searchType} className="marginB32">
+                <Radio value={1} >Keyword Search</Radio>
+                <Radio value={2} className="paddingL32" disabled={!isPayUser}>Lookalike Audience Search</Radio>
+              </Radio.Group>
+              <Form form={keyWordForm} name="keywords" className={searchType === 1 ? 'show' : 'hide'}>
+                <Form.Item name="keyWord" rules={[{required: true, message: 'Please input keyword!'}]}>
+                  <Select
+                    placeholder="Input keywords....."
+                    mode="tags"
+                    open={false}
+                    tokenSeparators={[',']}/>
+                </Form.Item>
+              </Form>
+              <Form
+                form={lookalikeForm}
+                name="lookalike"
+                layout="vertical"
+                onValuesChange={onLKSearchChange}
+                className={searchType === 2 ? 'show' : 'hide'}>
+                <Row gutter={32}>
+                  <Col span={12}>
+                    <Form.Item
+                      label="App ID"
+                      rules={[{required: true, message: 'Please input App ID!'}]}
+                      tooltip={{
+                        title: 'Tooltip with customize icon',
+                        icon: <InfoCircleOutlined/>,
+                      }}>
+                      <Input/>
+                    </Form.Item>
+                  </Col>
+                  <Col span={12}>
+                    <Form.Item
+                      label="App Secret"
+                      rules={[{required: true, message: 'Please input App secret!'}]}
+                      tooltip={{
+                        title: 'Tooltip with customize icon',
+                        icon: <InfoCircleOutlined/>,
+                      }}>
+                      <Input/>
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Form.Item
+                  label="Lookalike Audience ID"
+                  rules={[{required: true, message: 'Please input lookalike audience ID!'}]}
+                  tooltip={{
+                    title: 'Tooltip with customize icon',
+                    icon: <InfoCircleOutlined/>,
+                  }}
+                  name="audienceId"
+                >
+                  <Select
+                    placeholder="Custom Audience ID"
+                    dropdownRender={(menu) => (
+                      <div>
+                        {menu}
+                        <Divider style={{margin: '4px 0'}}/>
+                        <div style={{display: 'flex', flexWrap: 'nowrap', padding: 8}}>
+                          <Input
+                            style={{flex: 'auto'}}
+                            value={audienceID}
+                            onChange={(e) => setAudienceID(e.target.value)}
+                            maxLength={255}/>
+                          <a
+                            style={{flex: 'none', padding: '8px', display: 'block', cursor: 'pointer'}}
+                            onClick={addItem}
+                          >
+                            <PlusOutlined/> Add item
+                          </a>
+                        </div>
+                      </div>
+                    )}
+                  >
+                    {audienceIdItem.map((item) => (
+                      <Select.Option key={item.audienceId}>{item.audienceId}</Select.Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Form>
             </Card>
           </Col>
           <Col xl={6} md={7} xs={24} sm={24}>
@@ -146,7 +329,7 @@ const AudienceGenerator = ({userInfo, httpLoading, setHttpLoading}) => {
 
         <Row>
           <Col span={24} className="text-center">
-            <Space>
+            <Space size="large">
               <Button className="btn-xl">Reset All</Button>
               <Button type="primary" className="btn-xl">Generate Audience</Button>
             </Space>
