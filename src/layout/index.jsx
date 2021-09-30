811e1258c4d4e693901fd45c526f555efcc5bca3
Dongziwei <ziwei.dong@hcytech.com.cn>
2021-09-30 19:08:37 +0800

    Fixed some bugs

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -164,20 +164,21 @@ const Customlayout = ({history, activeKey, setLogged}) => {
         <div className="small-menu-close" onClick={()=>setmenuVisible(false)}><CloseOutlined /></div>
         <Menus/>
       </div>:null}
-      <Layout style={{minHeight: '100vh'}}>
-        <div className="sider-wrapper"><Sider
-          width={284}
-          breakpoint="lg"
-          trigger={null}
-          // collapsible={false}
-          collapsedWidth={0}
-          style={{minHeight: '100vh'}}
-        >
-          <div className="logoBox">
-            <a href="/"><img src={logo} width={168}/></a>
-          </div>
-          <Menus/>
-        </Sider>
+      <Layout style={{height: '100vh'}}>
+        <div className="sider-wrapper">
+          <Sider
+            width={284}
+            breakpoint="lg"
+            trigger={null}
+            // collapsible={false}
+            collapsedWidth={0}
+            style={{height: '100vh'}}
+          >
+            <div className="logoBox">
+              <a href="/home"><img src={logo} width={168}/></a>
+            </div>
+            <Menus/>
+          </Sider>
         </div>
         <Layout>
           <Header className="login-header">
